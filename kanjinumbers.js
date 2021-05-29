6c7d0594932d6587964898653d269a14f9645bdf
hariboten <botegineer@outlook.com>
2021-05-29 15:25:51 +0900

    #6 close, done

--- kanjinumbers.js
+++ kanjinumbers.js
@@ -1,13 +1,34 @@
-const kanji_nums = "零壱弐参四五六七八九"
+const	kanji_nums = "零壱弐参四五六七八九"
+const	kanji_digit = "零拾百千"
 
-function number2kanji (arg_str) {
-	const	arg_n = parseInt(arg_str, 10);
-	if (isNaN(arg_n) || arg_n < 0 || arg_n > 9) {
+function	n2k_ten_thousand_block (num)
+{
+
+}
+
+function	number2kanji (arg_str) {
+	let	arg_n = parseInt(arg_str, 10);
+	if (isNaN(arg_n) || arg_n < 0 || arg_n > 9999) {
 		console.log("invailed arg");
 		return (null);
 	}
-	let		kanji = "";
-	kanji = kanji_nums[arg_n] + kanji;
+	if (arg_n === 0)
+		return (kanji_nums[0]);
+	let	kanji = "";
+	for (let d = 0; d < 4; d++) {
+		let	digit_value = arg_n % 10;
+		if (digit_value == 0) {
+			arg_n = Math.floor(arg_n / 10);
+			continue ;
+		}
+		if (d != 0)
+			kanji = kanji_digit[d] + kanji;
+		if (digit_value != 1 || d == 0)
+			kanji = kanji_nums[digit_value] + kanji;
+		arg_n = Math.floor(arg_n / 10);
+		if (arg_n == 0)
+			break ;
+	}
 	return (kanji);
 }
 
