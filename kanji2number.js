df810bb7bbd421dc0a63711a005139a1eb8eafd7
hariboten <botegineer@outlook.com>
2021-05-29 16:42:27 +0900

    resolve #10

--- kanji2number.js
+++ kanji2number.js
@@ -1,20 +1,34 @@
 const	kanji_nums = "零壱弐参四五六七八九"
-const	kanji_digit = "零拾百千"
-const	kanji_big_digit = "零万億兆"
+const	kanji_digits = "零拾百千"
+const	kanji_big_digits = "零万億兆"
 const	kanji_strs = "零壱弐参四五六七八九拾百千万億兆"
 const	numbers_str = "0123456789"
 
 function kanji2number(arg_str)
 {
-	if (arg_str.length =! 1)
-		throw "invailed arg";
-	let	number = "";
-	let digit_value;
-	digit_value = kanji_nums.indexOf(arg_str);
-	if (digit_value < 0)
-		throw "invailed arg";
-	number = digit_value.toString();
-	return (number);
+	let	number = 0;
+	while (true) {
+		let digit_value;
+		let	digit;
+		digit_value = kanji_nums.indexOf(arg_str[0]);
+		if (digit_value < 0)
+			digit_value = 1;
+		else if (arg_str.length > 1)
+			arg_str = arg_str.substring(1);
+		else {
+			number += digit_value;
+			break ;
+		}
+		digit = kanji_digits.indexOf(arg_str[0]);
+		if (digit < 0)
+			digit = 0;
+		number += digit_value * (10 ** digit)
+		if (arg_str.length > 1)
+			arg_str = arg_str.substring(1);
+		else
+			break ;
+	}
+	return (number.toString());
 }
 
 console.log(kanji2number(process.argv[2]));
