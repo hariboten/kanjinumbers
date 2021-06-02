aefa681b8901b852d4826ff42c08970a0009ecee
hariboten <botegineer@outlook.com>
2021-06-02 19:47:31 +0900

    k2n v2, resolve #18

--- kanji2number.js
+++ kanji2number.js
@@ -1,34 +1,57 @@
 const	kanji_nums = "零壱弐参四五六七八九"
-const	kanji_digits = "零拾百千"
-const	kanji_big_digits = "零万億兆"
-const	kanji_strs = "零壱弐参四五六七八九拾百千万億兆"
+const	kanji_digits = "零拾百千"  // 10^i
+const	kanji_big_digits = "零万億兆"  // 10000^i
 const	numbers_str = "0123456789"
 
-function kanji2number(arg_str)
-{
-	let	number = 0;
-	while (true) {
-		let digit_value;
-		let	digit;
-		digit_value = kanji_nums.indexOf(arg_str[0]);
-		if (digit_value < 0)
-			digit_value = 1;
-		else if (arg_str.length > 1)
-			arg_str = arg_str.substring(1);
-		else {
-			number += digit_value;
+
+function	check_arg(arg) {
+	if (typeof arg !== "string")
+		throw "invailed arg";
+}
+
+function	kanji2number(kanji) {
+	check_arg(kanji);
+	if (kanji.length === 1 && kanji === kanji_nums[0]) // first, return simple case if value is 0. only this point expect character zero
+		return (numbers_str[0]);
+
+	let		number = 0n;  //using BigInt
+	let		store = 0; // 0 <= store <= 9999. store value of "4 digits block"
+	let		prev_d = 0; // store prev value not to backflow digits, like "ten bilion nine milion".
+	let		prev_big_d = 0;  // same as above
+
+	for (let p = 0; p < kanji.length; p++) {
+		let digit_value = kanji_nums.indexOf(kanji[p]);
+		if (digit_value <= 0)
+			throw "invailed arg";
+
+		p++;
+		if (!(p < kanji.length)) {
+			store += digit_value;
 			break ;
 		}
-		digit = kanji_digits.indexOf(arg_str[0]);
-		if (digit < 0)
-			digit = 0;
-		number += digit_value * (10 ** digit)
-		if (arg_str.length > 1)
-			arg_str = arg_str.substring(1);
-		else
-			break ;
+
+		let digit = kanji_digits.indexOf(kanji[p]);
+		if (digit > 0) {
+			if (prev_d !== 0 && digit > prev_d)
+				throw "invailed arg";
+			prev_d = digit;
+			store += digit_value * (10 ** digit);
+			continue ;
+		}
+
+		let big_digit = kanji_big_digits.indexOf(kanji[p]);
+		if (big_digit <= 0)
+			throw "invailed arg";
+		if (prev_big_d !== 0 && big_digit > prev_big_d)
+			throw "invailed arg";
+		prev_big_d = big_digit;
+		prev_d = 0;
+		store += digit_value;
+		number = number + BigInt(store) * (10000n ** BigInt(big_digit));
+		store = 0;
 	}
+	number = number + BigInt(store);
 	return (number.toString());
 }
 
-console.log(kanji2number(process.argv[2]));
+console.log(kanji2number(process.argv[2]));  // for print debug
